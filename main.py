9c9a8990e51b97688bf58005a3831b6dd1cb6be6
Lorenzo Moon <lhaskins@cecs.pdx.edu>
2024-08-01 14:39:43 -0700

    Init

---
+++ main.py
@@ -0,0 +1,21 @@
+###############################################################################
+#
+# Author: Lorenzo D. Moon
+# Professor: Anthony Rhodes
+# Course: CS-410 : Computer Vision & Deep Learning
+# Assignment: Programming Assignment 4
+# Description: Exercise #2: Grad-CAM DL-Based Saliency
+#
+###############################################################################
+
+import sys
+
+SETTINGS = {}
+
+
+def main(argv):
+    exit(0)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
